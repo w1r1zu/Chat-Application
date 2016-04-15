78d878564d7f0a821e0fa8be26591f1f5e4268d4
Will <wirizu@gmail.com>
2016-04-15 17:28:09 +0800

    Coded the functionality of the client application.

--- SocketApplicationClient/SocketApplicationClient/Form1.cs
+++ SocketApplicationClient/SocketApplicationClient/Form1.cs
@@ -7,14 +7,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.Sockets;
 
 namespace SocketApplicationClient
 {
     public partial class frmClient : Form
     {
+
+        System.Net.Sockets.TcpClient clientSocket = new System.Net.Sockets.TcpClient();
         public frmClient()
         {
             InitializeComponent();
+        }
+
+        private void frmClient_Load(object sender, EventArgs e)
+        {
+            Msg("Client started.");
+            clientSocket.Connect("127.0.0.1", 6969);
+            lblStatus.Text = "Client Socket Program - Connected to Server...";
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            NetworkStream serverStream = clientSocket.GetStream();
+            byte[] outStream = System.Text.Encoding.ASCII.GetBytes(txtMessage.Text + "$");
+            serverStream.Write(outStream, 0, outStream.Length);
+            serverStream.Flush();
+
+            byte[] inStream = new byte[10025];
+            serverStream.Read(inStream, 0, (int)clientSocket.ReceiveBufferSize);
+            string returndata = System.Text.Encoding.ASCII.GetString(inStream);
+            Msg(returndata);
+            txtMessage.Text = "";
+            txtMessage.Focus();
+        }
+
+        public void Msg(string msg)
+        {
+            txtServer.Text += txtServer.Text + Environment.NewLine + ">> " + msg;
         }
     }
 }
